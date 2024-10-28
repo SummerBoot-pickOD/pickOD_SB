2446313b670833d4ba01749ab48199b65751818e
Kardiem1102 <gabin1426@gmail.com>
2024-10-28 16:23:52 +0900

    241028 16:23 박가빈 초기 세팅(퍼블리싱 안넣었음)

---
+++ src/main/java/com/smbt/pickod/PickOdSbApplication.java
@@ -0,0 +1,13 @@
+package com.smbt.pickod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickOdSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickOdSbApplication.class, args);
+	}
+
+}
