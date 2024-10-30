0b58e17984256e52bb7869e7bc8d7b1070754ac7
Kardiem1102 <gabin1426@gmail.com>
2024-10-30 17:30:10 +0900

    241030 17:29 박가빈 db세팅 테스트 파일 설정

---
+++ src/main/java/com/smbt/pickod/mybatis/MyBatisConfig.java
@@ -0,0 +1,48 @@
+package com.smbt.pickod.mybatis;
+
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.RequiredArgsConstructor;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.type.JdbcType;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+@RequiredArgsConstructor
+public class MyBatisConfig {
+    private final ApplicationContext applicationContext;
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.hikari")
+    public HikariConfig hikariConfig() {
+        return new HikariConfig();
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        return new HikariDataSource(hikariConfig());
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory() throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource());
+
+        sqlSessionFactoryBean.setMapperLocations(applicationContext.getResources("classpath*:/mapper/**/*.xml")); // config.xml에 쓰지 않고도 dto랑 바로 연결하게
+        sqlSessionFactoryBean.setConfigLocation(applicationContext.getResource("classpath:config/mybatisConfig.xml"));
+        sqlSessionFactoryBean.setTypeAliasesPackage("com.example.finalApp.dto");
+
+        SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBean.getObject();
+        sqlSessionFactory.getConfiguration().setMapUnderscoreToCamelCase(true);
+        sqlSessionFactory.getConfiguration().setJdbcTypeForNull(JdbcType.NULL); // 누구를 null로 만든다는거지?
+
+        return sqlSessionFactory;
+    }
+}
