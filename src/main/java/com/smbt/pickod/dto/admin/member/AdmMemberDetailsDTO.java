8b408eee32b3e3f06c736577ff75e793bcbfb424
yujean-c <yujeanc.258@gmail.com>
2024-11-12 02:08:36 +0900

    20241112 02:07 최유진 회원 관리 페이지 검색 기능 구현 완료, 쪽지 전송 관련 서비스 및 매퍼 수정 완료, 관리자 비밀번호 찾기 삭제 완료

--- src/main/java/com/smbt/pickod/dto/admin/member/AdmMemberDetailsDTO.java
+++ src/main/java/com/smbt/pickod/dto/admin/member/AdmMemberDetailsDTO.java
@@ -17,9 +17,9 @@ public class AdmMemberDetailsDTO {
     private String memberAddress;
     private LocalDate memberBDate;
     private String memberGender;
+    private int totJournals;
+    private int totPlans;
     private int sanctionCnt;
-//    제재 여부의 경우, mapper 에서 CASE 문으로 'Y' / 'N' 로 해서 field 에 저장
     private String isBanned;
-//    HTML 에 제재 기간이 아니라 제재 종료일로 변경
     private String sanctionEndDate;
 }
