0e95dc1e617ebe10aa6f2599ae633f82fcfcd206
yujean-c <yujeanc.258@gmail.com>
2024-11-13 05:31:11 +0900

    20241113 05:30 최유진 장소 관리 페이지 DTO, mapper, service 페이지네이션 및 장소 댓글페이지 제외 작성 완료

--- src/main/java/com/smbt/pickod/mapper/admin/AdmPlaceMgmtMapper.java
+++ src/main/java/com/smbt/pickod/mapper/admin/AdmPlaceMgmtMapper.java
@@ -1,7 +1,23 @@
 package com.smbt.pickod.mapper.admin;
 
+import com.smbt.pickod.dto.admin.place.AdmPlaceDetailsDTO;
+import com.smbt.pickod.dto.admin.place.AdmPlaceFilterDTO;
+import com.smbt.pickod.dto.admin.place.AdmPlaceMgmtDTO;
+import com.smbt.pickod.dto.place.PlaceImgDTO;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface AdmPlaceMgmtMapper {
+    List<AdmPlaceMgmtDTO> getPlaces();
+
+    List<AdmPlaceMgmtDTO> filterPlaces(AdmPlaceFilterDTO admPlaceFilterDTO);
+
+    AdmPlaceDetailsDTO getPlaceDetail(Long placeId);
+
+    List<PlaceImgDTO> getPlaceDetailImg(Long placeId);
+
+    void deletePlace(Long placeId);
+
 }
